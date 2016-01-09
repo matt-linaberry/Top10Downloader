e86bf7e9cdb32bda4763793ddb6580056e4a8a05
Matt Linaberry <mtlinaberry@gmail.com>
2016-01-08 22:59:09 -0500

    Thru lesson 49!

--- app/src/main/java/edu/self/top10downloader/MainActivity.java
+++ app/src/main/java/edu/self/top10downloader/MainActivity.java
@@ -1,10 +1,18 @@
 package edu.self.top10downloader;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,5 +45,52 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+        // we are adding a child class to this main activity class
+        // this class is taking three parameters.
+        // 1st is the location, second will be a progress bar (traditionally), third is a result string.
+        private String mFileContents;  // store the text download
+        // elippsis is a variable list!
+        @Override
+        protected String doInBackground(String... params) {
+            // this is what needs to be run in the background
+            mFileContents = downloadXMLFile(params[0]);
+            if (mFileContents == null) {
+                Log.d("DownloadData", "Error downloading");
+            }
+            return mFileContents;
+        }
+
+        private String downloadXMLFile(String urlPath) {
+            // biffer to store the file.
+            StringBuilder tempBuffer = new StringBuilder();
+            try {
+                // open the URL
+                URL url = new URL(urlPath);
+                HttpURLConnection connection = (HttpURLConnection)url.openConnection();
+                int response = connection.getResponseCode();
+                Log.d("DownloadData", "The response code is " + response);
+                InputStream is = connection.getInputStream();
+                InputStreamReader isr = new InputStreamReader(is);
+
+                int charRead;
+                char[] inputBuffer = new char[500];  // alloc 500 byte chunks.
+                while (true) {
+                    charRead = isr.read(inputBuffer);
+                    if (charRead <= 0) {
+                        break;
+                    }
+                    tempBuffer.append(String.copyValueOf(inputBuffer, 0, charRead));
+
+                }
+
+                return tempBuffer.toString(); // return ALL contents.
+            }
+            catch (IOException e) {
+                Log.d("DownloadData", "IOException reading data: " + e.getMessage());
+            }
+        }
     }
 }
